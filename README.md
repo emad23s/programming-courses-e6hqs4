4ed75eeb18eb9623dce48271816df80dbcc56f90
emad23s <emadkhateeb19@gmail.com>
2021-10-04 14:04:33 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# programming-courses-e6hqs4
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/programming-courses-e6hqs4)
---
+++ index.ts
@@ -0,0 +1,21 @@
+//######################################################################
+//###########################           SSTech       ###################
+//###########################   Programming Courses  ###################
+//######################################################################
+let num1: number;
+let num2: number;
+let num3: number;
+let num4: number;
+let num5: number;
+let num6: number;
+num1 = 10;
+num2 = 15;
+num3 = num1 + num2;
+num4 = 20;
+num5 = num1 + num2 + num3 + num4;
+num6 = num1 * num2;
+console.log(num3);
+console.log(num5);
+console.log(num6);
+
+
